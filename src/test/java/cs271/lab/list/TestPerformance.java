918df0129ac1750d55301675e2620446db15da85
SALMANUDDIN TALHA MOHD <mohdsalmanuddintalha@gmail.com>
2025-02-03 16:16:02 -0600

    I have done all and TODO part in the code  and test with different sizes and recorded the times as well as all the test cases within the source code are passed successfully.

--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -17,7 +17,6 @@ public class TestPerformance {
   // TODO (optional) refactor to DRY
   // which of the two lists performs better as the size increases?
   private final int SIZE = 10;
-
   // TODO choose this value in such a way that you can observe an actual effect
   // for increasing problem sizes
   private final int REPS = 1000000;
@@ -41,36 +40,106 @@ public class TestPerformance {
     arrayList = null;
     linkedList = null;
   }
-
+  private long recordTime(Runnable test) {
+    long startTime = System.nanoTime();
+    test.run();
+    return System.nanoTime() - startTime;
+  }
   @Test
   public void testLinkedListAddRemove() {
+    long startTime = System.nanoTime();
     for (var r = 0; r < REPS; r++) {
       linkedList.add(0, 77);
       linkedList.remove(0);
-    }
+      }
+      long endTime = System.nanoTime();
+      System.out.println("LinkedList Add/Remove Time: " + (endTime - startTime) + " ns");
+
   }
 
   @Test
   public void testArrayListAddRemove() {
+    long startTime = System.nanoTime();
     for (var r = 0; r < REPS; r++) {
-      arrayList.add(0, 77);
-      arrayList.remove(0);
+      arrayList.add(0, 77); // Adding at the beginning
+      arrayList.remove(0);  // Removing from the beginning
     }
+    long endTime = System.nanoTime();
+    System.out.println("ArrayList Add/Remove Time: " + (endTime - startTime) + " ns");
   }
 
   @Test
   public void testLinkedListAccess() {
     var sum = 0L;
+    long startTime = System.nanoTime();
     for (var r = 0; r < REPS; r++) {
-      sum += linkedList.get(r % SIZE);
+      sum += linkedList.get(r % SIZE);  // Accessing elements in a round-robin fashion
     }
+    long endTime = System.nanoTime();
+    System.out.println("LinkedList Access Time: " + (endTime - startTime) + " ns");
   }
 
   @Test
   public void testArrayListAccess() {
     var sum = 0L;
+    long startTime = System.nanoTime();
     for (var r = 0; r < REPS; r++) {
-      sum += arrayList.get(r % SIZE);
+      sum += arrayList.get(r % SIZE);  // Accessing elements in a round-robin fashion
+    }
+    long endTime = System.nanoTime();
+    System.out.println("ArrayList Access Time: " + (endTime - startTime) + " ns");
+  }
+  @Test
+  public void testPerformanceWithDifferentSizes() {
+    // Try different sizes and run the performance tests for each
+    int[] sizes = {10, 100, 1000, 10000};
+    for (int size : sizes) {
+      System.out.println("Running tests for SIZE = " + size);
+
+      // Resize the lists accordingly
+      arrayList = new ArrayList<>(size);
+      linkedList = new LinkedList<>();
+      for (int i = 0; i < size; i++) {
+        arrayList.add(i);
+        linkedList.add(i);
+      }
+
+      // Test Add/Remove
+      long linkedListAddRemoveTime = recordTime(() -> {
+        for (var r = 0; r < REPS; r++) {
+          linkedList.add(0, 77);
+          linkedList.remove(0);
+        }
+      });
+      long arrayListAddRemoveTime = recordTime(() -> {
+        for (var r = 0; r < REPS; r++) {
+          arrayList.add(0, 77);
+          arrayList.remove(0);
+        }
+      });
+
+      // Test Access
+      long linkedListAccessTime = recordTime(() -> {
+        long sum = 0L;
+        for (var r = 0; r < REPS; r++) {
+          sum += linkedList.get(r % size);
+        }
+      });
+      long arrayListAccessTime = recordTime(() -> {
+        long sum = 0L;
+        for (var r = 0; r < REPS; r++) {
+          sum += arrayList.get(r % size);
+        }
+      });
+
+      // Output the results
+      System.out.printf("SIZE = %d\n", size);
+      System.out.printf("LinkedList Add/Remove Time: %d ns\n", linkedListAddRemoveTime);
+      System.out.printf("ArrayList Add/Remove Time: %d ns\n", arrayListAddRemoveTime);
+      System.out.printf("LinkedList Access Time: %d ns\n", linkedListAccessTime);
+      System.out.printf("ArrayList Access Time: %d ns\n", arrayListAccessTime);
+      System.out.println("-----------------------------------");
     }
   }
+
 }
